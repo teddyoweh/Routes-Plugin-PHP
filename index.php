6aeb26dde9c3ba317c0e1a1ee5e6dfc152c9ec40
Ifechukwudeni Oweh <76883580+teddyoweh@users.noreply.github.com>
2022-05-08 00:41:04 -0500

    update

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$route = new Route();
+$route->add('/users','test/users.php'); // Adding /users to the route
+$route->add('/register','test/reg.php'); // Adding /register to the route
+ 
+$route->init(); // Starting the routing
+ 
+?>
--- routes.php
+++ routes.php
@@ -2,27 +2,33 @@
  
  
 class Route{
-    public $routes =array();
-    public $notfound ='';
+    public $routes =array(); // Empty Route Dictionary that would contain all routes and pages ( file )
+    public $notfound =''; // Empty Not Found Variable
     
-    public function add(string $location,string $file){
-        $this->routes[$location] = $file;
+    public function add(string $location,string $file){ // Adding Routes Function 
+
+        $this->routes[$location] = $file; // Assigning routes and pages ( files ) as key and item in a dictionary
 
     }
+    public function error404($notfoundpath){ // Not Found ( Error 404 ) Function
+        
+        $this->notfound = $notfoundpath; // Assigning the notfound path variable to a page 
 
-    public function init()
+    }
+    public function init()  // initializing all the routes
     {
     
-        foreach ($this->routes as $path => $content) {
-            if ($path == $_SERVER['PATH_INFO']) {
+        foreach ($this->routes as $path => $content) {  // Reading the Dictionary off all routes and the pages ( file )
+
+            if ($path == $_SERVER['PATH_INFO']) { // If the path in the dictionary is the current location of the website
                 
-               include $content;
+               include $content; // Including the file assigned to the path 
                  
             } 
                
         
         }
-        if(in_array($_SERVER['PATH_INFO'],$this->routes)){
+        if(in_array($_SERVER['PATH_INFO'],$this->routes)){ // If the current location of the website is no in the dictionary
             if(strlen($this->notfound)!==0) {
                 include $this->notfound;
             }else{
