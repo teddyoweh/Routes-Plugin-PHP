13ac1407c72e06913fe15b77d14ec57a0f7e9d1f
Ifechukwudeni Oweh <76883580+teddyoweh@users.noreply.github.com>
2022-05-08 02:41:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 $route = new Route();
+$route->add('/','test/app.php'); // Home Page
 $route->add('/users','test/users.php'); // Adding /users to the route
 $route->add('/register','test/reg.php'); // Adding /register to the route
-$route->error404('test/error.php');
+$route->error404('test/error.php'); Adding 440
 $route->init(); // Starting the routing
  
 ?>
