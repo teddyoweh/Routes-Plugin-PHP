3705f0d70e7b66c6f9afeb556b0a7b1a115f9bc6
Ifechukwudeni Oweh <76883580+teddyoweh@users.noreply.github.com>
2022-05-08 01:05:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $route = new Route();
 $route->add('/users','test/users.php'); // Adding /users to the route
 $route->add('/register','test/reg.php'); // Adding /register to the route
-$route->erro
+$route->error4
 $route->init(); // Starting the routing
  
 ?>
