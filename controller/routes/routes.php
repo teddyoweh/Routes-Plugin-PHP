b69b4c4dbe574b779ec6ea518487e7a090bbf3f4
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2022-05-16 01:01:12 -0500

    Update routes.php

--- controller/routes/routes.php
+++ controller/routes/routes.php
@@ -73,8 +73,8 @@ class Route{
     public function loadboxicon($loca){ // Adding Scripts Function
         if ($loca == 'web'){
             echo '
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+           <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet">
+           <script src="https://unpkg.com/boxicons@2.1.2/dist/boxicons.js"></script>
             ';
 
         }else if ($loca == 'local'){
@@ -84,7 +84,7 @@ class Route{
                     echo "<link rel='stylesheet' href='$this->boxiconsstylelocation/$value' />";
                 } 
             }
-             
+            echo "<script defer src='$this->scriptstaticlocation/boxicon.js'></script>";
             
         }
     }
