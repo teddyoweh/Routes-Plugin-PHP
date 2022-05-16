a180e2e0e556f5c23f7a974d016ad9cbe8a9a8c8
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2022-05-16 03:39:55 -0500

    update

--- install.py
+++ install.py
@@ -7,16 +7,18 @@ class RouteSet:
         self.ostype =  platform.system()
         self.url = 'https://github.com/teddyoweh/Routes-Plugin-PHP/archive/refs/heads/main.zip'
         self.windowscmdinstall ='powershell -c "Invoke-WebRequest -Uri {}  -OutFile ./file.zip" '.format(self.url)
-        self.windowscmdunzip = 'Expand-Archive -LiteralPath file.zip -DestinationPath ./app'
+        self.windowscmdunzip = 'powershell Expand-Archive -LiteralPath file.zip -DestinationPath ./'
         
         
-        if self.
+        if str(self.ostype).lower() == 'windows':
+            self.windows()
     def windows(self):
         os.system(self.windowscmdinstall)
-        os.mkdir('app')
+        
+        os.system('mkdir app')
         os.system(self.windowscmdunzip)
         os.system('cd app')
-        shutil.move('Routes-Plugin-PHP-main','./')
+        os.rename('Routes-Plugin-PHP-main','app')
         
         os.system('')
-print("OS in my system : ",my_os)
+RouteSet()
