5efdafb90ef365b23a62ca804480655b17b8f164
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2022-05-15 21:28:48 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
-<?php
-$route = new Route();
-$route->add('/','test/app.php'); // Home Page
-$route->add('/users','test/users.php'); // Adding /users to the route
-$route->add('/register','test/reg.php'); // Adding /register to the route
-$route->error404('test/error.php'); // Adding 404 Error Page
-$route->init(); // Starting the routing
  
-?>
 
 <?php
 require('./controller/routes/routes.php');
-$route = new Route();
+$route = new Route(); 
 
 // Stylesheets and scripts
 $route->addscripts('script.js');
