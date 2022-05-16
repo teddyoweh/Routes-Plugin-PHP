288a0fdcd70548948a0cf51142fa449f8f130387
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2022-05-15 21:23:53 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,29 @@ $route->init(); // Starting the routing
  
 ?>
 
-This plugin can be embedded in PHP application to give the web application specific routes/href location
-https://github.com/teddyoweh/Routes-Plugin-PHP
+<?php
+require('./controller/routes/routes.php');
+$route = new Route();
 
-#php #backenddeveloper #programming #plugins #Python #pythonprogramming 
+// Stylesheets and scripts
+$route->addscripts('script.js');
+$route->addstylesheets('style.scss');
+$route->loadbootstrap('local');
+$route->loadpyscripts('local');
+
+// Routings
+
+// Basic Pages
+$route->homepage('home.php'); // Adding Home Page
+$route->error404('error404.php'); // Adding 404 Error Page
+
+// Other Pages
+$route->add('/users','users.php');
+$route->add('/register','reg.php');
+$route->init(); // Starting the routing
+
+
+
+// All Pages are located in  ./controller/pages
+?>
+ 
